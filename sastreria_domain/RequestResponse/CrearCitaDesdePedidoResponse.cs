ded1fef99470b01833df02de9746eee837c8343b
Pancito <pansiendopan@gmail.com>
2026-01-27 12:27:41 -0500

    Create CrearCitaDesdePedidoResponse.cs

---
+++ sastreria_domain/RequestResponse/CrearCitaDesdePedidoResponse.cs
@@ -0,0 +1,8 @@
+namespace sastreria_domain.RequestResponse
+{
+    public class CrearCitaDesdePedidoResponse
+    {
+        public int IdCita { get; set; }
+        public DateTime FechaHora { get; set; }
+    }
+}
