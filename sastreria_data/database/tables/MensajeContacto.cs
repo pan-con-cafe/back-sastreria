bb1cbb6384e415f2087b41b6940b8c66b8110d66
Pancito <pansiendopan@gmail.com>
2026-02-07 01:12:56 -0500

    Create MensajeContacto.cs

---
+++ sastreria_data/database/tables/MensajeContacto.cs
@@ -0,0 +1,16 @@
+public class MensajeContacto
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Asunto { get; set; } = null!;
+
+    public string Mensaje { get; set; } = null!;
+
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    public bool Leido { get; set; } = false;
+}
